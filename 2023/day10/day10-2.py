98f092c76504cd5eccf0653653a160218b7828e4
Benedikt Otto <b.otto.code@protonmail.com>
2023-12-11 11:55:51 +0100

    day10

---
+++ 2023/day10/day10-2.py
@@ -0,0 +1,248 @@
+#!/usr/bin/python3
+# Advent of Code 2023 - Day 10, Part 2
+# Benedikt Otto
+import math
+
+# Open puzzle file.
+#with open('example_10-4.txt') as file:
+with open('input_10.txt') as file:
+    lines = file.readlines()
+
+# Assuming rectangular area (accounting for newline \n).
+height = len(lines)
+width = len(lines[0]) - 1
+
+# Parse input into dict with key=Tuple(coords) and val=Symbol
+pipes = {}
+for row, line in enumerate(lines):
+    for col, s in enumerate(line.strip()):
+        pipes[(row, col)] = s
+
+
+def connectable(coord, direction):
+    """Checks if the pipe at coords can be connected in the direction."""
+    is_connectable = False
+    (row, col) = coord
+
+    next_coord = (0, 0)
+    viable_symbols = []
+    match direction:
+        case "NORTH":
+            next_coord = (row - 1, col)
+            viable_symbols = ['|', '7', 'F', 'S']
+        case "SOUTH":
+            next_coord = (row + 1, col)
+            viable_symbols = ['|', 'L', 'J', 'S']
+        case "EAST":
+            next_coord = (row, col + 1)
+            viable_symbols = ['-', 'J', '7', 'S']
+        case "WEST":
+            next_coord = (row, col - 1)
+            viable_symbols = ['-', 'L', 'F', 'S']
+
+    # Connectable only if the pipe in that direction is within the rectangle
+    # and it is one of the viable symbols.
+    if 0 <= next_coord[0] < height and 0 <= next_coord[1] < width:
+        is_connectable = pipes[next_coord] in viable_symbols
+
+    return is_connectable
+
+
+# Build graph as dict(key=coords, val=[neighbors])
+graph = {}
+for coord in pipes:
+    (row, col) = coord
+    north_coord = (row - 1, col)
+    south_coord = (row + 1, col)
+    west_coord = (row, col - 1)
+    east_coord = (row, col + 1)
+
+    neighbors = []
+    # TODO: refactor using a dict of directions.
+    match pipes[coord]:
+        case '|':
+            if connectable(coord, "NORTH"):
+                neighbors.append(north_coord)
+            if connectable(coord, "SOUTH"):
+                neighbors.append(south_coord)
+        case '-':
+            if connectable(coord, "EAST"):
+                neighbors.append(east_coord)
+            if connectable(coord, "WEST"):
+                neighbors.append(west_coord)
+        case 'F':
+            if connectable(coord, "EAST"):
+                neighbors.append(east_coord)
+            if connectable(coord, "SOUTH"):
+                neighbors.append(south_coord)
+        case '7':
+            if connectable(coord, "SOUTH"):
+                neighbors.append(south_coord)
+            if connectable(coord, "WEST"):
+                neighbors.append(west_coord)
+        case 'J':
+            if connectable(coord, "WEST"):
+                neighbors.append(west_coord)
+            if connectable(coord, "NORTH"):
+                neighbors.append(north_coord)
+        case 'L':
+            if connectable(coord, "NORTH"):
+                neighbors.append(north_coord)
+            if connectable(coord, "EAST"):
+                neighbors.append(east_coord)
+        case 'S':
+            # Special case for the start 'S', could connect in all directions.
+            if connectable(coord, "NORTH"):
+                neighbors.append(north_coord)
+            if connectable(coord, "SOUTH"):
+                neighbors.append(south_coord)
+            if connectable(coord, "EAST"):
+                neighbors.append(east_coord)
+            if connectable(coord, "WEST"):
+                neighbors.append(west_coord)
+            # Also remember S as the source node.
+            source_node = coord
+    graph.setdefault(coord, neighbors)
+
+
+# Determine distance of all nodes to source using the Dijkstra-Algorithm.
+#   dist[v] = distance from source to v
+#   prev[v] = previous vertex that has the shortest distance to v
+non_visited = []
+dist = {}
+prev = {}
+for v in graph:
+    # Prepare dicts.
+    dist.setdefault(v, math.inf)
+    prev.setdefault(v, None)
+    non_visited.append(v)
+dist[source_node] = 0
+
+while len(non_visited) > 0:
+    # Get vertex with minimum dist:
+    minimum = math.inf
+    for v in non_visited:
+        if dist[v] < minimum:
+            minimum = dist[v]
+            min_vertex = v
+
+    if minimum == math.inf:
+        break  # Here the remaining nodes are unreachable.
+
+    non_visited.remove(min_vertex)
+
+    # Work through all neighbors of min_vertex.
+    for n in graph[min_vertex]:
+        # In this graph all edge values are 1.
+        alt = dist[min_vertex] + 1
+        if alt < dist[n]:
+            dist[n] = alt
+            prev[n] = min_vertex
+
+# Get node with the maximum non-infinity value.
+max_steps = 0
+steps_node = None
+for v in dist:
+    steps = dist[v]
+    if steps != math.inf:
+        max_steps = max(steps, max_steps)
+
+print(f'Max Steps are {max_steps}.')
+
+
+# We need a (ordered) list of elements on the loop:
+next_node = graph[source_node][1]
+pipes_loop = [source_node, next_node]
+
+while next_node != source_node:
+    node = pipes_loop[-1]
+    neighbors = graph[node]
+    new_nodes = [n for n in graph[node] if n not in pipes_loop]
+    if len(new_nodes) > 0:
+        next_node = new_nodes[0]
+    else:
+        break
+    pipes_loop.append(next_node)
+
+# Only non-loop pipes can be inner pipes.
+pipes_nonloop = [v for v in graph if v not in pipes_loop]
+
+# Replace the starting pipe 'S' with the right pipe as part of the loop.
+source_neighbors = [pipes_loop[-1], pipes_loop[1]]
+source_north = (source_node[0] - 1, source_node[1])
+source_south = (source_node[0] + 1, source_node[1])
+source_west = (source_node[0], source_node[1] - 1)
+source_east = (source_node[0], source_node[1] + 1)
+source_symbol = 'S'
+if source_north in source_neighbors:
+    if source_south in source_neighbors:
+        source_symbol = '|'
+    elif source_east in source_neighbors:
+        source_symbol = 'L'
+    elif source_west in source_neighbors:
+        source_symbol = 'J'
+elif source_south in source_neighbors:
+    if source_east in source_neighbors:
+        source_symbol = 'F'
+    elif source_west in source_neighbors:
+        source_symbol = '7'
+
+pipes[source_node] = source_symbol
+
+# For each non-loop pipe we determine whether it is an inner or outer pipe and count them.
+sum_inner = 0
+for coord in pipes_nonloop:
+    (row, col) = coord
+
+    # Put all the pipes north of coord in a list.
+    north_line = []
+    for k in range(row):
+        north_line.append((k, col))
+
+    # Walk through all symbols north of the coord and count how many times we cross the loop:
+    #   > hitting a '-' always counts as a crossing, and '|' never counts as one.
+    #   > for corners '7'/'L'/'F'/'J', we need to remember whether the loop came 'from right/left'
+    #
+    #   |      |  walking dir.  |       |
+    #   V      V                V       V
+    #
+    # --7      F--            --7       F--
+    #   |      |      vs.       |       |
+    #   L--  --J              --J       L--
+    #   crossings              no crossings
+    #
+    from_left = False
+    from_right = False
+
+    crossing_count = 0
+    for coord in north_line:
+        if coord not in pipes_loop:
+            continue  # we only care about hitting loop symbols.
+
+        crossing_symbol = pipes[coord]
+        match crossing_symbol:
+            case '-':
+                crossing_count += 1  # '-' is always a crossing
+            case '7':
+                from_left = True  # not a crossing yet, but we remember the dir. we came from.
+            case 'F':
+                from_right = True  # not a crossing yet, but we remember the dir. we came from.
+            case 'L':
+                # Only a crossing if we came from left.
+                if from_left:
+                    crossing_count += 1
+                    from_left = False
+                from_right = False
+            case 'J':
+                # Only a crossing if we came from right.
+                if from_right:
+                    crossing_count += 1
+                    from_right = False
+                from_left = False
+
+    # The point is an inner point exactly when there is an odd number of crossings
+    # needed to reach the edge of the area.
+    if crossing_count % 2 == 1:
+        sum_inner += 1
+
+print(f'Number of inner pipes: {sum_inner}')
